d1cf768e4db8a2252d58f80f3b5f5ed0254204cb
sayujshah <sayujshah@gmail.com>
2024-10-15 22:33:28 -0500

    Initial prompt testing on a snippet of CoDA dataset

--- MAD_CTI.py
+++ MAD_CTI.py
@@ -2,9 +2,17 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-from autogen import ConversableAgent, register_function
+from autogen import AssistantAgent, ConversableAgent, GroupChat, GroupChatManager, register_function
 from DarkWebScraper import DarkWebScraper
 
+from datasets import load_dataset
+import pandas as pd
+
+# Login using e.g. `huggingface-cli login` to access this dataset
+os.getenv('HF_TOKEN')
+ds = load_dataset("s2w-ai/CoDA", split="train")
+ds_snippet = ds[0:5]
+      
 llm_config = {
     'config_list':
     [
@@ -15,9 +23,29 @@ llm_config = {
     ]
 }
 
-assistant = ConversableAgent(
-    name="Dark Web Scraper",
-    system_message="You are a scraper agent that crawls a provided list of dark web URLs and extract its contents.",
+assistant1 = AssistantAgent(
+    name="Dark_Web_Text_Scraper",
+    system_message=
+    """
+    You are a scraper agent that reads through text found on the dark web and cleans them to ensure they are easy to comprehendable for other agents.
+    You will be provided with a DatasetDict object that has the following features: '__key__', '__url__', and 'txt'.
+    Your job is to clean the texts found in the 'txt' feature by extracting only the most relevant aspects of the text.
+    Leave the rest of the structure of the DatasetDict object alone. Output the DatasetDict object with the cleaned 'txt' values to the next agent.
+    """,
+    llm_config=llm_config
+
+)
+
+assistant2 = AssistantAgent(
+    name="Text_Context_Classifier",
+    system_message=
+    """
+    You will take in a DatasetDict object containing keys, URLs, and dark-web-related text.
+    Your job is to analyze each text and determine whether they have to do with a dark web forum conversation.
+    You will return a list of the URLs for each text and their associated classification: 'Forum' or 'Not Forum',
+    where 'Forum' means you think the context of the text is part of a dark web forum conversation and 'Not Forum' is when it does not have to do with a forum conversation.
+    Note that if the text looks like a listing of forum posts, this does NOT count as part of the 'Forum' classification. We are only interested in individual forum postings themselves.
+    """,
     llm_config=llm_config
 )
 
@@ -28,17 +56,16 @@ user_proxy = ConversableAgent(
     human_input_mode="NEVER"
 )
 
-register_function(
-    DarkWebScraper,
-    caller=assistant,
-    executor=user_proxy,
-    name="DarkWebScraper",
-    description="A dark web scraper/crawler"
+groupchat = GroupChat(agents=[assistant1, assistant2, user_proxy], messages=[], max_round=12)
+manager = GroupChatManager(groupchat=groupchat, llm_config=llm_config)
+
+chat_result = user_proxy.initiate_chat(
+    manager, message=
+    f"""
+    Pass the DatasetDict object at the end of this message onto the Dark_Web_Text_Scraper agent.
+    You will take the outputs of the Dark Web Text Scraper agent and pass it onto the Text_Context_Classifier agent.
+    When the Text_Context_Classifier agent is complete with its task, it will output a list of URLs and their classification ('Forum' or 'Not Forum').
+    Output this list within the terminal in a easy-to-read format, along with the word 'TERMINATE' at the end of the list.
+    Use the following DatasetDict object to complete your specified project: {ds_snippet}.
+    """
 )
-
-urls_test = [
-    'http://ransomwr3tsydeii4q43vazm7wofla5ujdajquitomtd47cxjtfgwyyd.onion',
-    'http://dreadytofatroptsdj6io7l3xptbet6onoyno2yv7jicoxknyazubrad.onion/post/c12ec91069be2b9ef93b' # Ransomware post
-        ]
-
-chat_result = user_proxy.initiate_chat(assistant, message=f"Scrape through each .onion URL within {urls_test} and extract the website's contents. Output each URL's extracted content within the terminal.")
